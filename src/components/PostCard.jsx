aefd4f9ee679b37b0fcb91ff047cd890293ad438
Jisan <jisan@Macs-MacBook-Pro-2.local>
2023-05-26 21:20:19 +0600

    first practise

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,8 @@
+export const Postcard = ()=>{
+    return (
+<>      
+        <h1>This is our First Task</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate animi porro doloremque libero quos veniam, rem ratione quidem, alias, labore laboriosam et error beatae qui magnam facere dolor corporis eos. </p>
+</>
+    )
+}
